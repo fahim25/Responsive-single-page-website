77e1c43a9858b2ca7cb9e99dd95d3893e0952d55
fahim25 <fahimmuntasir201@gmail.com>
2020-08-08 12:15:32 +0600

    bxslider js and custom script

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+-----------------------------
+|   | Bxslider 
+-----------------------------
+*/
+$(function () {
+    $('.bxslider').bxSlider({
+        slideWidth:350,
+        auto: true,
+        minSlide:1,
+        maxSlide:3,
+        slideMargin:50
+    });
+}); 
