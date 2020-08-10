3fb73780a5d3e646c24b638932454f62a19635a9
fahim25 <fahimmuntasir201@gmail.com>
2020-08-10 16:47:56 +0600

    navigation

--- js/script.js
+++ js/script.js
@@ -24,3 +24,113 @@ $(function () {
         time: 2000
     });
 });
+
+/*
+-----------------------------
+|   | Navbar
+-----------------------------
+*/
+       
+
+$(function () {
+    showHideNav();
+    $(window).scroll(function () {
+        showHideNav();
+    });
+
+    function showHideNav() {
+        if ($(window).scrollTop() < 250) {
+            $('.navbar').css({
+                'margin-top':'-100px',
+                'opacity':'0'
+            });
+            
+            $('.navbar-default').css({
+                'background-color':'rgba(59,59,59,0)'
+            });
+
+        } else {
+
+            $('.navbar').css({
+                'margin-top':'0',
+                'opacity':'1'
+            });
+            
+            $('.navbar-default').css({
+                'background-color':'rgba(59,59,59,0.7)'
+            });
+        }
+    }
+});
+
+
+/*
+====================================
+|   |   |   | Smooth Scroll
+====================================
+*/
+
+
+$(function () {
+
+   $('.nav-link, #scroll-to-top')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { 
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); 
+            $target.focus(); 
+          };
+        });
+      }
+    }
+  });
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
