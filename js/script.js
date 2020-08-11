fc709bdcd49e621806c8cdbd1b82dfff192d8286
fahim25 <fahimmuntasir201@gmail.com>
2020-08-11 14:50:13 +0600

    header padding fixed

--- js/script.js
+++ js/script.js
@@ -115,14 +115,29 @@ $(function () {
 });
 
 
+/*
+====================================
+|   |   |   | Header Container fix
+====================================
+*/
 
-
-
-
-
-
-
-
+$(document).ready(function() {
+    setInterval( function() {
+        
+        var windowHeight = $(window).height();
+        
+        var containerHeight = $(".header-container").height();
+        
+        var padTop = windowHeight - containerHeight;
+        
+        $(".header-container").css({
+            
+            'padding-top': Math.round( padTop / 2) + 'px',
+            'padding-bottom': Math.round( padTop / 2) + 'px'
+            
+        });
+    }, 10)
+});
 
 
 
